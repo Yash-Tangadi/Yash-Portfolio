7343b98af9f5bd06fab70ff461c7723540d2f95d
Yash Tangadi <yashtangadi1999@gmail.com>
2024-04-28 00:45:23 +0530

    added folders and seperated out the css and javascript code for better structure of the project

---
+++ js/index.js
@@ -0,0 +1,7 @@
+<script>
+    var typed = new Typed('#element', {
+        strings: ['Fullstack Java Developer', 'Gaming Content Creator', 'Video Editor', 'Freelancer', 'Fullstack Java Developer', 'Gaming Content Creator', 'Video Editor', 'Freelancer', 'Fullstack Java Developer', 'Gaming Content Creator', 'Video Editor', 'Freelancer', 'Fullstack Java Developer', 'Gaming Content Creator', 'Video Editor', 'Freelancer', 'Fullstack Java Developer', 'Gaming Content Creator', 'Video Editor', 'Freelancer', 'Fullstack Java Developer', 'Gaming Content Creator', 'Video Editor', 'Freelancer'],
+    typeSpeed: 60,
+});
+
+</script>
